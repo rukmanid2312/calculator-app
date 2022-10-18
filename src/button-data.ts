894d64a3dd83c301896397c8e14f33c0dc5c7521
rukmanid2312 <110535673+rukmanid2312@users.noreply.github.com>
2022-10-19 02:22:21 +0530

    Add files via upload

---
+++ src/button-data.ts
@@ -0,0 +1,182 @@
+const btnMisc = {
+    bgColor: '#b4b4b4',
+    color: 'black'
+  }
+
+  const btnOperator = {
+    bgColor: '#ff9900',
+    color: 'white'
+  }
+
+  const btnDigit = {
+    bgColor: '#737373',
+    color: 'white'
+  }
+export const numpad = [
+    {
+      name: "AC",
+      value: "AC",
+      bg: btnMisc.bgColor,
+      color: btnMisc.color,
+      w: '40px',
+      h: '40px',
+      br: '50%'
+    },
+    {
+      name: " C",
+      value: "C",
+      bg: btnMisc.bgColor,
+      color: btnMisc.color,
+      w: '40px',
+      h: '40px',
+      br: '50%'
+    },
+    {
+      name: "+/-",
+      value: "SIGN",
+      bg: btnMisc.bgColor,
+      color: btnMisc.color,
+      w: '40px',
+      h: '40px',
+      br: '50%'
+    },
+    {
+      name: "÷",
+      value: "/",
+      bg: btnOperator.bgColor,
+      color: btnOperator.color,
+      w: '40px',
+      h: '40px',
+      br: '50%'
+    },
+    {
+      name: " 7",
+      value: "7",
+      bg: btnDigit.bgColor,
+      color: btnDigit.color,
+      w: '40px',
+      h: '40px',
+      br: '50%'
+    },
+    {
+      name: "8",
+      value: "8",
+      bg: btnDigit.bgColor,
+      color: btnDigit.color,
+      w: '40px',
+      h: '40px',
+      br: '50%'
+    },
+    {
+      name: "9",
+      value: "9",
+      bg: btnDigit.bgColor,
+      color: btnDigit.color,
+      w: '40px',
+      h: '40px',
+      br: '50%'
+    },
+    {
+      name: "x",
+      value: "*",
+      bg: btnOperator.bgColor,
+      color: btnOperator.color,
+      w: '40px',
+      h: '40px',
+      br: '50%'
+    },
+    {
+      name: "4",
+      value: "4",
+      bg: btnDigit.bgColor,
+      color: btnDigit.color,
+      w: '40px',
+      h: '40px',
+      br: '50%'
+    },{
+      name: "5",
+      value: "5",
+      bg: btnDigit.bgColor,
+      color: btnDigit.color,
+      w: '40px',
+      h: '40px',
+      br: '50%'
+    },{
+      name: "6",
+      value: "6",
+      bg: btnDigit.bgColor,
+      color: btnDigit.color,
+      w: '40px',
+      h: '40px',
+      br: '50%'
+    },
+    {
+      name: "-",
+      value: "-",
+      bg: btnOperator.bgColor,
+      color: btnOperator.color,
+      w: '40px',
+      h: '40px',
+      br: '50%'
+    },{
+      name: "1",
+      value: "1",
+      bg: btnDigit.bgColor,
+      color: btnDigit.color,
+      w: '40px',
+      h: '40px',
+      br: '50%'
+    },{
+      name: "2",
+      value: "2",
+      bg: btnDigit.bgColor,
+      color: btnDigit.color,
+      w: '40px',
+      h: '40px',
+      br: '50%'
+    },
+    {
+      name: "3",
+      value: "3",
+      bg: btnDigit.bgColor,
+      color: btnDigit.color,
+      w: '40px',
+      h: '40px',
+      br: '50%'
+    },{
+      name: "+",
+      value: "+",
+      bg: btnOperator.bgColor,
+      color: btnOperator.color,
+      w: '40px',
+      h: '40px',
+      br: '50%'
+    },
+    {
+      name: "0",
+      value: "0",
+      bg: btnDigit.bgColor,
+      color: btnDigit.color,
+      w: '85px',
+      h: '40px',
+      br: '20px'
+    },
+    {
+      name: ".",
+      value: ".",
+      bg: btnDigit.bgColor,
+      color: btnDigit.color,
+      w: '40px',
+      h: '40px',
+      br: '50%'
+    },
+    {
+      name: "=",
+      value: "=",
+      bg: btnOperator.bgColor,
+      color: btnOperator.color,
+      w: '40px',
+      h: '40px',
+      br: '50%'
+    }
+  ]
