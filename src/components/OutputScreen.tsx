aa046339f8c17836f71e81df71d03609ec974912
rukmanid2312 <110535673+rukmanid2312@users.noreply.github.com>
2022-10-19 00:09:22 +0530

    Update OutputScreen.tsx

--- src/components/OutputScreen.tsx
+++ src/components/OutputScreen.tsx
@@ -1,8 +1,6 @@
-
-
-import React, { FunctionComponent } from 'react'
-import styled from 'styled-components'
-import { Box,VStack,HStack } from "@chakra-ui/layout";
+import { FunctionComponent } from 'react'
+import { Box, VStack } from "@chakra-ui/layout";
+import { Flex } from '@chakra-ui/react';
 
 interface OutputScreenProps {
   expression: string
@@ -13,13 +11,11 @@ interface OutputScreenProps {
 
 export const OutputScreen: FunctionComponent<OutputScreenProps> = ({ value, expression }) => {
   return (
- 
-    <VStack>
-       <Box> {expression}</Box>  
-       <Box>  {value}</Box>  
-      </VStack>
-       
-      
+    <VStack display='flex' alignItems='end' justifyContent='end' pr="35px" pb='20px' h="140px">
+      <Box color='white'> {expression}</Box>
+      <Box color='white'>  {value}</Box>
+
+    </VStack >
   )
 }
 
